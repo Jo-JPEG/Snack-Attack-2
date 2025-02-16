aa5294e9d56435bb330ee06f10296fea45405a22
Jo <joanne.c.wright@googlemail.com>
2025-02-16 14:44:35 +0000

    add tictactoe app and update snake.html to only show game when logged in

--- codevipers/urls.py
+++ codevipers/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('', include('snake.urls')),  # Include the snake app's URLs
+    path('tictactoe/', include('tictactoe.urls')),
 ]
