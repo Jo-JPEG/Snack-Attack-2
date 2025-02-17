bacf463d5c04d33914ac1122060002814fa3dba9
Harmeetkaur13 <iharmeetk@gmail.com>
2025-02-17 11:21:19 +0000

    add cloudinary

--- snake/models.py
+++ snake/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,7 +8,7 @@ class Profile(models.Model):
 
 class Game(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image = models.ImageField(upload_to='static/snake', blank=True)
+    image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.name
