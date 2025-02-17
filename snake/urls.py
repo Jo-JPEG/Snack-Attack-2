c779b9e97368844a5d4fe601dc52a8260caff538
Jo <joanne.c.wright@googlemail.com>
2025-02-17 16:28:04 +0000

    update views for tictactoe index

--- snake/urls.py
+++ snake/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('submit_score/', views.submit_score, name='submit_score'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('tictactoe/', views.tictactoe, name='tictactoe'),
-    path('game/<str:game_name>/', views.game_welcome, name='game_welcome'),  
+    path('game/<str:game_name>/', views.game_welcome, name='game_welcome'),
+    path('tictacindex/', views.tictacindex, name='tictacindex'),   
 ]
